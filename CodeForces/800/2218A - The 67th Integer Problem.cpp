3ed3ac3aa72964732df8e266c2cb729bf145be71
Ariful Islam <iariful2002@gmail.com>
2026-04-05 16:31:45 +0600

    2218A - The 67th Integer Problem

---
+++ CodeForces/800/2218A - The 67th Integer Problem.cpp
@@ -0,0 +1,17 @@
+/*
+Problem :   2218A - The 67th Integer Problem
+Link    :   https://codeforces.com/problemset/problem/2218/A
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<x<<endl;
+    }
+    return 0;
+}
