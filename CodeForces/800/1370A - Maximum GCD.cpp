3a3acfb990b71c0e19a74ab7da6fbc46e0489e66
Ariful Islam <iariful2002@gmail.com>
2026-04-11 23:09:05 +0600

    1370A - Maximum GCD

---
+++ CodeForces/800/1370A - Maximum GCD.cpp
@@ -0,0 +1,18 @@
+/*
+Problem :   1370A - Maximum GCD
+Link    :   https://codeforces.com/problemset/problem/1370/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<(n/2)<<endl;
+    }
+    return 0;
+}
